bbb4b9bcd6b565dc24b5a5cfe9642cd9355a225f
izdoughty <izdoughty@gmail.com>
2021-01-12 21:18:46 -0600

    Intial Commit

---
+++ Assets/heroControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heroControl : MonoBehaviour { 
+    public float xVelAdj;
+    public float yVelAdj;
+    public float xFire;
+    public float yFire;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xVelAdj = Input.GetAxis("xMove");
+        yVelAdj = Input.GetAxis("yMove");
+
+        GetComponent<Rigidbody>().velocity = new Vector3 (3 * xVelAdj, 3 * yVelAdj, 0);
+    }
+}
